b91a8c510fb4d97fc919b0a548a3e70f280539d6
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2024-10-19 11:58:29 +1000

    wip vite config to adhere to chromium ext requirements

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        popup: "index.html",
+        background: "src/background.ts", // event-driven services
+      },
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+
+    outDir: "dist", // chromium ext expects 'dist' folder
+  },
+});
