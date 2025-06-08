8fc07f31511822b48696d0f025ef8af568428f6e
johnny <74356539+jjjjony@users.noreply.github.com>
2025-06-08 22:32:59 +1000

    add functionality to padlock button

--- chromium-extension/src/lib/components/PadLockButton.tsx
+++ chromium-extension/src/lib/components/PadLockButton.tsx
@@ -3,14 +3,14 @@ import { usePinStore } from "~/lib/stores/PinStore";
 import { Button } from "~/lib/ui/shadcn/button";
 
 export default function PadLockButton() {
-  const { pinStatus } = usePinStore();
+  const { pinStatus, lock } = usePinStore();
 
   if (pinStatus === "SETTING_UP") {
     return null;
   }
 
   return (
-    <Button variant="secondary" size="icon" className="size-8 absolute top-0 left-0 z-50">
+    <Button onClick={lock} disabled={pinStatus !== "UNLOCKED"} variant="secondary" size="icon" className="size-8 absolute top-0 left-0 z-50">
       {pinStatus === "LOCKED" ? <LockIcon /> : <LockOpenIcon />}
     </Button>
   );
