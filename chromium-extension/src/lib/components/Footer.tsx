d43ea81ae0751986ec05bff56f786af04b4267b9
johnny <74356539+jjjjony@users.noreply.github.com>
2025-06-13 19:38:19 +1000

    wip footer reacting to theme

--- chromium-extension/src/lib/components/Footer.tsx
+++ chromium-extension/src/lib/components/Footer.tsx
@@ -1,11 +1,19 @@
+import { Theme } from "../enums/Theme";
+import { useTheme } from "../hooks/useTheme";
+import { cn } from "../utils/cn";
 import githubLogo from "/images/github-logo.svg";
 
-// TODO: we invert the GH logo when we toggle theme, but GPT a way to not let this invert the selection color (or just don't make the icon selectable)
 export default function Footer() {
+  const { theme } = useTheme();
+
   return (
-    <footer className="sticky bottom-0 w-full backdrop-blur-sm p-4 flex justify-center">
+    <footer className="sticky bottom-0 w-full p-4 flex justify-center">
       <a href="https://vitejs.dev" target="_blank">
-        <img src={githubLogo} className="logo invert h-4" alt="Vite logo" />
+        <img
+          src={githubLogo}
+          className={cn("select-none pointer-events-auto h-4", theme !== Theme.dark && "invert")}
+          alt="GitHub repo"
+        />
       </a>
     </footer>
   );
