32f256361b0a1382688e6dfe695ec5154465b233
johnny <74356539+jjjjony@users.noreply.github.com>
2025-06-14 15:44:27 +1000

    fallback error message

--- chromium-extension/src/lib/utils/console-helpers.ts
+++ chromium-extension/src/lib/utils/console-helpers.ts
@@ -1,9 +1,10 @@
+const FALLBACK_ERROR_MESSAGE = "Unknown";
 /**
  * Consistent way to log errors to console
  * Might be redundant; more useful if we have an external telemetry service
  */
 export function logError(error: unknown, prefix?: string): string {
-  const cleanErrorMessage = error instanceof Error ? error.message : "Unknown";
+  const cleanErrorMessage = error instanceof Error ? (error.message ?? FALLBACK_ERROR_MESSAGE) : FALLBACK_ERROR_MESSAGE;
   const finalMessage =
     prefix?.trim() !== "" ? `${prefix}, reason: ${cleanErrorMessage}` : `Error: ${cleanErrorMessage}`;
   console.error(finalMessage, error);
