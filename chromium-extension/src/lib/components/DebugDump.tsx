c9907870c7243b8800962585588a215f1d4990df
johnny <74356539+jjjjony@users.noreply.github.com>
2025-06-15 10:07:19 +1000

    wip

--- chromium-extension/src/lib/components/DebugDump.tsx
+++ chromium-extension/src/lib/components/DebugDump.tsx
@@ -1,12 +1,14 @@
-import { usePinStore } from "../stores/PinStore";
-import { useUserPreferencesStore } from "../stores/UserPreferencesStore";
+import { usePinStore } from "~/lib/stores/PinStore";
+import { useUserPreferencesStore } from "~/lib/stores/UserPreferencesStore";
 
 const SHARED_STYLES = "text-left select-text z-[9999] text-xs max-w-[200px] overflow-x-scroll";
+
 export default function DebugDump() {
   const pinStoreJson = usePinStore((state) => state.GET_DEBUG_JSON_DUMP);
   const userPreferencesStoreJson = useUserPreferencesStore((state) => state.GET_DEBUG_JSON_DUMP);
 
   // TODO: move to dialog?
+  // TODO: this isn't re-rendering when the store changes FYI, it just gets a snapshot so maybe this should be a dialog when open takes a snapshot? and renders?
   return (
     <>
       <pre className={SHARED_STYLES}>{pinStoreJson()}</pre>
