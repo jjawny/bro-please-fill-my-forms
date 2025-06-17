412c1dcb22ec1dfd6b8f22c755f130f268fa4292
johnny <74356539+jjjjony@users.noreply.github.com>
2025-06-17 14:38:12 +1000

    resolve todo

--- chromium-extension/src/lib/components/Footer.tsx
+++ chromium-extension/src/lib/components/Footer.tsx
@@ -1,14 +1,15 @@
 import { usePinStore } from "~/lib/hooks/stores/usePinStore";
 import { useUserPreferencesStore } from "~/lib/hooks/stores/useUserPreferencesStore";
 
+/**
+ * Place to log misc/generic/global errors rather than using toasts (as this is an extension)
+ */
 export default function Footer() {
   const pinStoreFatalError = usePinStore((state) => state.fatalError);
   const userPreferencesStoreFatalError = useUserPreferencesStore((state) => state.fatalError);
 
   return (
     <footer className="flex-grow sticky items-end w-full flex z-[9999] justify-center">
-      {/* TODO: make this nice for the user
-      as this is a chrome extension, do we use toasts or just display the latest error at bottom of screen? (toasts are probably bad UX in chrome extensions) */}
       {pinStoreFatalError && <span className="text-red-500 ml-2">Pin Store Error: {pinStoreFatalError}</span>}
       {userPreferencesStoreFatalError && (
         <span className="text-red-500 ml-2">UserPreferences Store Error: {userPreferencesStoreFatalError}</span>
