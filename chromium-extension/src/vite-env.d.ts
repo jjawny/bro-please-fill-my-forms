a843361c7783cbd4b550f30c1692aeab37ee1192
johnny <74356539+jjjjony@users.noreply.github.com>
2025-06-19 09:30:31 +1000

    allow importing markdown files

--- chromium-extension/src/vite-env.d.ts
+++ chromium-extension/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+/**
+ * Allow markdown files to be imported as strings
+ */
+declare module "*.md" {
+  const markdown: string;
+  export { markdown };
+}
--- chromium-extension/vite.config.ts
+++ chromium-extension/vite.config.ts
@@ -2,9 +2,10 @@ import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
+import { plugin as mdPlugin, Mode } from "vite-plugin-markdown";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), mdPlugin({ mode: [Mode.MARKDOWN] })],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
