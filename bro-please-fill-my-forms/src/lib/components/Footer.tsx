97d718249096c5cdb5a899b3741b3171f936c53b
johnny <74356539+jjjjony@users.noreply.github.com>
2025-06-23 09:24:25 +1000

    ability to close global errors

--- bro-please-fill-my-forms/src/lib/components/Footer.tsx
+++ bro-please-fill-my-forms/src/lib/components/Footer.tsx
@@ -1,14 +1,33 @@
+import { XIcon } from "lucide-react";
 import { useGlobalStore } from "~/lib/hooks/stores/useGlobalStore";
+import { RippleButton } from "./shadcn/ripple";
 
 /**
  * Place to log misc/generic/global errors rather than using toasts (as this is a CRX)
  */
 export default function Footer() {
   const globalError = useGlobalStore((state) => state.globalError);
+  const setGlobalError = useGlobalStore((state) => state.setGlobalError);
 
+  const clearGlobalError = () => {
+    setGlobalError(undefined);
+  };
   return (
     <footer className="flex-grow sticky items-end w-full flex justify-center h-4">
-      {globalError && <span className="text-red-500 text-sm">{globalError}</span>}
+      {globalError && (
+        <div className="flex items-center gap-1">
+          <span className="text-red-500 text-sm">{globalError}</span>
+          <RippleButton
+            title="Close error"
+            size="icon"
+            variant="destructive"
+            onClick={clearGlobalError}
+            className="size-5"
+          >
+            <XIcon />
+          </RippleButton>
+        </div>
+      )}
     </footer>
   );
 }
