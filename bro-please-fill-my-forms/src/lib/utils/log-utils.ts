6872c618e543a590cd411284215d8f0dcdd0ca68
johnny <74356539+jjjjony@users.noreply.github.com>
2025-07-05 12:36:44 +1000

    only log errors during dev

--- bro-please-fill-my-forms/src/lib/utils/log-utils.ts
+++ bro-please-fill-my-forms/src/lib/utils/log-utils.ts
@@ -10,7 +10,10 @@ export function logError(error: unknown, prefix?: string): string {
   const cleanErrorMessage = error instanceof Error ? (error.message ?? FALLBACK_ERROR_MESSAGE) : FALLBACK_ERROR_MESSAGE;
   const finalMessage =
     prefix?.trim() !== "" ? `${prefix}, reason: ${cleanErrorMessage}` : `Error: ${cleanErrorMessage}`;
-  console.error(finalMessage, error);
+
+  if (import.meta.env.DEV) {
+    console.error(finalMessage, error);
+  }
   return finalMessage;
 }
 
